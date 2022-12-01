ed7f2261f5b272355d711cb7660552ca760c7903
Taichi93258 <taiponpon0308@gmail.com>
2022-12-02 03:57:03 +0900

    ToDoリスト表示ページの変更

--- resources/views/todo_list.blade.php
+++ resources/views/todo_list.blade.php
@@ -1,3 +1,20 @@
+<head>
+    @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+</head>
+
+
 <h1>post List</h1>                                                                                                 
 <div>
     <h2>タスクの一覧</h2>
@@ -10,6 +27,8 @@
             <th>見積時間(h)</th>
             <th colspan="2">操作</th>
         </tr>
+
+        @if(@isset($posts)) 
         @foreach($posts as $post)
         <tr>
             <td>{{$post->task_name}}</td>
@@ -20,8 +39,12 @@
             <td><a href="/delete-page/{{$post->id}}">削除</a></td>
         </tr>
         @endforeach
+        @endif
     </table>
+
+    @if(isset($estimate_hour_sum))
     <p>
         見積時間の合計(h):{{$estimate_hour_sum}}
     </p>
+    @endif
 </div>
