eb2011324c67a70ecb5c0c1f09477e66cbabca7a
Taichi93258 <taiponpon0308@gmail.com>
2022-12-09 10:49:48 +0900

    共通関数の作成

---
+++ app/Library/Estimation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Library;
+
+use Illuminate\Support\Facades\Facade;
+
+class Estimation extends Facade
+{
+    public function estimate($posts)
+    {
+        $estimate_hour_sum = 0;
+        foreach ($posts as $post) {
+            $estimate_hour_sum += $post->estimate_hour;
+        }
+        return $estimate_hour_sum;
+    }
+}
