0de076246c06a8a15c3d8169e7656d91b4bcee56
Taichi93258 <taiponpon0308@gmail.com>
2022-12-18 19:37:34 +0900

    モデルに関数を定義する

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Requests\PostRequest;
 
 class Post extends Model
 {
@@ -13,5 +14,30 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo('App\Models\User');
+    }
+
+    public function GetUser()
+    {
+        return $this->with('user')->get();
+    }
+
+    public function GetLoginUser()
+    {
+        return $this->where('user_id', auth()->id())->get();
+    }
+
+    public function StorePost(PostRequest $request)
+    {
+        $this->create($request->all() + ['user_id' => auth()->id()]);
+    }
+
+    public function UpdatePost(PostRequest $request)
+    {
+        $this->update($request->all());
+    }
+
+    public function DeletePost()
+    {
+        $this->delete();
     }
 }
