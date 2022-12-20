ef80ec955716eb16d7dd105d9489eb42b1458d0a
Taichi93258 <taiponpon0308@gmail.com>
2022-12-21 02:42:05 +0900

    usersテーブルからreleaseカラムを削除

--- database/migrations/2022_12_21_011620_remove_release_from_users_table.php
+++ database/migrations/2022_12_21_011620_remove_release_from_users_table.php
@@ -13,7 +13,7 @@ return new class () extends Migration {
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('release')->after('id');
+            $table->dropColumn('release');
         });
     }
 
@@ -25,7 +25,7 @@ return new class () extends Migration {
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('release');
+            $table->integer('release')->after('id');
         });
     }
 };
