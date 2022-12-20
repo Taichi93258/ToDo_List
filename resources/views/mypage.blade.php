fb1b0683338caad65489a867d05397d29fdedbc6
Taichi93258 <taiponpon0308@gmail.com>
2022-12-21 02:44:55 +0900

    マイページから公開設定ができるようにする

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,3 +1,12 @@
+<head>
+    @if (Route::has('login'))
+        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            @auth
+                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+            @endauth
+        </div>
+    @endif
+</head>
 <h1>MyPage</h1>
 <div>
     <a href="/posts/create">タスクを追加</a>
@@ -9,6 +18,7 @@
             <th>見積時間(h)</th>
             <th>投稿時間</th>
             <th>優先度</th>
+            <th>公開設定</th>
             <th colspan="2">操作</th>
         </tr>
 
@@ -21,29 +31,29 @@
                     <td>{{ $post->estimate_hour }}</td>
                     <td>{{ $post->created_at }}</td>
                     <td>{{ App\Enums\Priority::from($post->priority)->label() }}</td>
+                    <td>
+                        <form method="POST" action="{{ route('posts.release', ['post' => $post->id]) }}" id="release">
+                            @csrf
+                            <div class="form-group">
+                                @foreach (App\Enums\Release::cases() as $release)
+                                    <input type='radio' name="release" value="{{ $release->value }}"
+                                        @checked(old('priority', $post->release) == $release->value)>
+                                    {{ $release->label() }}</option>
+                                @endforeach
+                            </div>
+                        </form>
+                    </td>
                     <td><a href="{{ route('posts.edit', ['post' => $post->id]) }}">編集</a></td>
                     <td><a href="{{ route('posts.delete', ['post' => $post->id]) }}">削除</a></td>
                 </tr>
             @endforeach
         @endisset
     </table>
+    <input type="submit" form="release" value="公開設定を変更">
 
     @isset($estimate_hour_sum)
         <p>
             見積時間の合計(h):{{ $estimate_hour_sum }}
         </p>
     @endisset
-    <form method="POST" action="{{ route('users.release', ['user' => $post->user->id]) }}">
-        @csrf
-        <div class="form-group">
-            <label for="release-field">公開設定</label>
-            <select name="release" id="release-field">
-                @foreach (App\Enums\Release::cases() as $release)
-                    <option value="{{ $release->value }}" @selected(old('priority', $post->user->release) == $release->value)>
-                        {{ $release->label() }}</option>
-                @endforeach
-            </select>
-        </div>
-        <input type="submit" name="change" value="変更を反映">
-    </form>
 </div>
