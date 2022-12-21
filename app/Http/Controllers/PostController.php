9caccd0c69b54dadfcce7bf2c1d1edc0db6b1c6e
Taichi93258 <taiponpon0308@gmail.com>
2022-12-21 16:37:07 +0900

    updatePostメソッドの利用

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -115,7 +115,7 @@ class PostController extends Controller
 
     public function release(Request $request, Post $post)
     {
-        $post->updateUserRelease($request);
+        $post->updatePost($request);
         return redirect()->route('posts.index');
     }
 }
