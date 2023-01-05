054a3e4ea4055d47a5852ff007535252562ebfc6
Taichi93258 <taiponpon0308@gmail.com>
2023-01-05 14:35:14 +0900

    postsテーブルに固定データを追加

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Enums\Release;
 
 class PostsTableSeeder extends Seeder
 {
@@ -16,6 +17,35 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('posts')->insert([
+            [
+                'user_id' => 11,
+                'release' => Release::PUBLIC->value,
+                'task_name'      => 'テスト1',
+                'task_description'  => 'テストデータです',
+                'estimate_hour' => 10,
+                'priority' => fake()->numberBetween(1, 3),
+            ],
+
+            [
+                'user_id' => 11,
+                'release' => Release::PUBLIC->value,
+                'task_name'      => 'テスト2',
+                'task_description'  => 'テストデータです',
+                'estimate_hour' => 10,
+                'priority' => fake()->numberBetween(1, 3),
+            ],
+
+            [
+                'user_id' => 11,
+                'release' => Release::PUBLIC->value,
+                'task_name'      => 'テスト3',
+                'task_description'  => 'テストデータです',
+                'estimate_hour' => 10,
+                'priority' => fake()->numberBetween(1, 3),
+            ]
+            ]);
+
         //Post::factory()->count(10)->create();
     }
 }
