e81a401ccb9a681557d69659913413fd06b899e3
Taichi93258 <taiponpon0308@gmail.com>
2023-01-05 14:32:59 +0900

    サービスコンテナに登録

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Library\Estimation;
+use App\Library\GetTags;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         app()->bind('Estimation', Estimation::class);
+        app()->bind('GetTags', GetTags::class);
     }
 
     /**
