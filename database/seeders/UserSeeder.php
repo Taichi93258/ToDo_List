03f0045b83ca73d3352577eaec2683f9f71d6f83
Taichi93258 <taiponpon0308@gmail.com>
2023-01-05 14:36:11 +0900

    usersテーブルに固定データ追加

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -18,5 +21,15 @@ class UserSeeder extends Seeder
         User::factory(10)
             ->hasPosts(3)
             ->create();
+
+        DB::table('users')->insert([
+            [
+                'name'           => '會澤大智',
+                'email'          => 'test@gmail.com',
+                'email_verified_at' => '2022-12-30 07:19:5',
+                'password'       => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ]
+            ]);
     }
 }
