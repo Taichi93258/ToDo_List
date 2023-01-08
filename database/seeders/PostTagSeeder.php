ca341562b572bb8d62de266e645435b0b9911b2a
Taichi93258 <taiponpon0308@gmail.com>
2023-01-08 12:48:11 +0900

    コメントアウト削除

--- database/seeders/PostTagSeeder.php
+++ database/seeders/PostTagSeeder.php
@@ -18,18 +18,15 @@ class PostTagSeeder extends Seeder
     public function run()
     {
         for ($i = 0; $i < Post::count(); $i++) {
-            // postsとtagsテーブルのidカラムをランダムに並び替え、先頭の値を取得
             $set_post_id = Post::select('id')->orderByRaw("RAND()")->first()->id;
             $set_tag_id = Tag::select('id')->orderByRaw("RAND()")->first()->id;
 
-            // クエリビルダを利用し、上記のモデルから取得した値が、現在までの複合主キーと重複するかを確認
             $post_tag = DB::table('post_tag')
                             ->where([
                                 ['post_id', '=', $set_post_id],
                                 ['tag_id', '=', $set_tag_id]
                             ])->get();
 
-            // 上記のクエリビルダで取得したコレクションが空の場合、外部キーに上記のモデルから取得した値をセット
             if ($post_tag->isEmpty()) {
                 DB::table('post_tag')->insert(
                     [
@@ -41,20 +38,5 @@ class PostTagSeeder extends Seeder
                 $i--;
             }
         }
-
-        // DB::table('post_tag')->insert([
-        //     [
-        //         'post_id' => 31,
-        //         'tag_id' => 1,
-        //     ],
-        //     [
-        //         'post_id' => 32,
-        //         'tag_id' => 2,
-        //     ],
-        //     [
-        //         'post_id' => 33,
-        //         'tag_id' => 3,
-        //     ]
-        // ]);
     }
 }
