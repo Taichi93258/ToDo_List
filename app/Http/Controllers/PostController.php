4108cbf54816ac3a54a8283ba363c816259ff681
Taichi93258 <taiponpon0308@gmail.com>
2023-01-09 17:06:22 +0900

    タグが選択されていない場合、viewにはnullではなく空配列を渡す

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
     {
         $posts = $post->fetchPostWithTags($request);
         $estimate_hour_sum = FacadeEstimation::estimate($posts);
-        $select_tags = $request->tags;
+        $select_tags = $post->checkSelectTags($request);
 
         return view('todo/list', compact('posts', 'estimate_hour_sum', 'select_tags'));
     }
